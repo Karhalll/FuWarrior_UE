94d82f43abacafd311cf51df4d3029f2b671b230
Martin Fučík <mar.fucik@gmail.com>
2021-11-26 09:53:52 +0100

    Custom GameMode and PlayerController

---
+++ Source/FuWarrior_UE/FuWarrior_UEPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "FuWarrior_UEPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FUWARRIOR_UE_API AFuWarrior_UEPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
